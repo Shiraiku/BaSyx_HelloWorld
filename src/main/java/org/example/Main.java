176cdc391db21e4752fb77206ce3d5ff7a044d44
Philippe Barbie <philippe.barbie@iese.fraunhofer.de>
2026-04-01 14:54:38 +0200

    HelloWorld example for AAS and Submodel : Create, Get, Edit, Delete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,51 +3,85 @@ package org.example;
 import org.eclipse.digitaltwin.aas4j.v3.model.*;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.*;
 import org.eclipse.digitaltwin.basyx.aasenvironment.client.ConnectedAasManager;
+import org.eclipse.digitaltwin.basyx.aasservice.client.ConnectedAasService;
+import org.eclipse.digitaltwin.basyx.submodelservice.client.ConnectedSubmodelService;
 
 import java.util.List;
-import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
-    AssetAdministrationShell helloAAS= new DefaultAssetAdministrationShell.Builder()
-            .id("http://example.com/aas/helloWorld")
-            .idShort("HelloWorldAAS")
-            .build();
+        //SEE POM.XML FOR NECESSARY DEPENDENCY BASYX LIBRARIES AND AAS4J OBJECT MODEL
+        //THIS CODE NEEDS BASYX [AAS REPOSITORY] , [SUBMODEL REPOSITORY] , [AAS REGISTRY] and [SUBMODEL REGISTRY] RUNNING
+        //SEE basyx.org FOR BASYX OFF THE SHELF DOCKER CONTAINER DOWNLOADS
 
-    List<SubmodelElement> helloProperties = List.of(
-            new DefaultProperty.Builder()
-                    .idShort("SomeNewProperty")
-                    .value("Hello")
-                    .build());
+        //Connect to AAS Environment (BaSyx Servers)
+        String aasRegistryBaseUrl = "http://localhost:8082";
+        String aasRepositoryBaseUrl = "http://localhost:8081";
+        String submodelRegistryBaseUrl = "http://localhost:8083";
+        String submodelRepositoryBaseUrl = "http://localhost:8081";
+        ConnectedAasManager helloManager = new ConnectedAasManager(aasRegistryBaseUrl, aasRepositoryBaseUrl, submodelRegistryBaseUrl, submodelRepositoryBaseUrl);
 
-    DefaultKey key = new DefaultKey.Builder().type(KeyTypes.CONCEPT_DESCRIPTION).value("https://admin-shell.io/zvei/nameplate/2/0/HelloWorld").build();
-    DefaultReference ref = new DefaultReference.Builder().type(ReferenceTypes.EXTERNAL_REFERENCE).keys(key).build();
+        //Create AAS in Java Object Model
+        AssetAdministrationShell helloAAS= new DefaultAssetAdministrationShell.Builder()
+                .id("http://example.com/aas/helloWorld")
+                .idShort("HelloWorldAAS")
+                .build();
 
-    Submodel helloSubmodel = new DefaultSubmodel.Builder()
-            .id("http://example.com/aas/helloWorld/submodel")
-            .idShort("Submodel1")
-            .submodelElements(helloProperties)
-            .semanticId(ref)
-            .build();
+        //Create SemanticId for Submodel
+        DefaultKey key = new DefaultKey.Builder().type(KeyTypes.SUBMODEL).value("http://example.com/aas/helloWorld/submodel").build();
+        DefaultReference ref = new DefaultReference.Builder().type(ReferenceTypes.EXTERNAL_REFERENCE).keys(key).build();
 
-    String aasRegistryBaseUrl = "http://localhost:8082";
-    String aasRepositoryBaseUrl = "http://localhost:8081";
-    String submodelRegistryBaseUrl = "http://localhost:8083";
-    String submodelRepositoryBaseUrl = "http://localhost:8081";
-    ConnectedAasManager helloManager = new ConnectedAasManager(aasRegistryBaseUrl, aasRepositoryBaseUrl, submodelRegistryBaseUrl, submodelRepositoryBaseUrl);
+        //Create empty Submodel in Java Object Model
+        Submodel helloSubmodel = new DefaultSubmodel.Builder()
+                .id("http://example.com/aas/helloWorld/submodel")
+                .idShort("helloSubmodel")
+                .semanticId(ref)
+                .build();
 
-    try{
-    helloManager.createAas(helloAAS);
-    }catch(Exception e){ System.out.println(e);} //Problem: Exception in AAS-Registry: 409 obwohl neu
+        //Push AAS from Java to BaSyx AAS-Repository and register in AAS-Registry
+        try{
+        helloManager.createAas(helloAAS);
+        }catch(Exception e){ System.out.println(e);} //Problem: Exception in AAS-Registry: 409 obwohl neu
 
-    try {
-        helloManager.createSubmodelInAas(helloAAS.getId(), helloSubmodel);
-    }catch(Exception e){ System.out.println(e);} //Problem: Exception in Submodel-Registry: 409 obwohl neu : Es wird danach KEINE Verknpüpfung zwischen AAS und Submodel gemacht!
+        //Push Submodel from Java to BaSyx Submodel-Repository and register in Submodel-Registry
+        try {
+            helloManager.createSubmodelInAas(helloAAS.getId(), helloSubmodel);
+        }catch(Exception e){ System.out.println(e);} //Problem: Exception in Submodel-Registry: 409 obwohl neu : Es wird danach KEINE Verknpüpfung zwischen AAS und Submodel gemacht!
 
-    helloManager.deleteAas(helloAAS.getId());
+        //Initialize Service Managers
+        ConnectedAasService helloAASService = helloManager.getAasService(helloAAS.getId());
+        ConnectedSubmodelService helloSMService = helloManager.getSubmodelService(helloSubmodel.getId());
 
+        //Embed Submodel Reference into AAS
+        helloAASService.addSubmodelReference(helloSubmodel.getSemanticId());
+
+        //Create new SubmodelElementCollection (SMC) in Java Object Model
+        SubmodelElementCollection helloCollection =  new DefaultSubmodelElementCollection.Builder()
+                .idShort("helloSMCollection")
+                .value(List.of())
+                .build();
+
+        //Add SubmodelElementCollection (SMC) from Java Object to Submodel
+        helloSMService.createSubmodelElement(helloCollection);
+
+        //Get Submodel Element from Submodel Service (Submodel Server) and add a Property to the SMC (Update), then re-push it to the Server
+        SubmodelElementCollection helloSMC = (SubmodelElementCollection) helloSMService.getSubmodelElement("helloSMCollection");
+        helloSMC.getValue().add(
+                new DefaultProperty.Builder().idShort("helloProperty").valueType(DataTypeDefXsd.STRING).value("h3ll0World!").build());
+        helloSMService.updateSubmodelElement("helloSMCollection", helloSMC);
+
+        //STOP EXECUTION HERE TO SEE RESULTS
+        Integer addBreakpointHere;
+        //REST OF EXAMPLE CODE WILL DELETE EVERYTHING CREATED
+
+        //Delete the Property
+        helloSMService.deleteSubmodelElement("helloSMCollection.helloProperty");
+
+        //Delete whole Submodel
+        helloManager.deleteSubmodelOfAas(helloAAS.getId(),helloSubmodel.getId());
+
+        //Delete whole AAS
+        helloManager.deleteAas(helloAAS.getId());
     }
 }
